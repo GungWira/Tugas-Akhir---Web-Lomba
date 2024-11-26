5942f7247d6bb00bd87841f653abcb4a6787290b
AbiMantra132 <103944081+AbiMantra132@users.noreply.github.com>
2024-11-27 01:30:34 +0800

    Abi: Server install prisma & buat module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CompetitionsModule } from './competitions/competitions.module';
+import { ReimbursementModule } from './reimbursement/reimbursement.module';
+import { TeamsModule } from './teams/teams.module';
+import { AchievementModule } from './achievement/achievement.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CompetitionsModule, ReimbursementModule, TeamsModule, AchievementModule],
   controllers: [AppController],
   providers: [AppService],
 })
